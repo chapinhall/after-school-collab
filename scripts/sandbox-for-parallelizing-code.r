dffd138a6acd6a52bc9474bf2982913564da5d87
nsmader <nsmader@gmail.com>
2014-12-08 14:30:18 -0600

    Added more to parallelization sandbox

--- scripts/sandbox-for-parallelizing-code.r
+++ scripts/sandbox-for-parallelizing-code.r
@@ -6,17 +6,43 @@
 
 # For reference
 # http://www.r-bloggers.com/parallel-r-loops-for-windows-and-linux/
+# http://www.r-bloggers.com/the-wonders-of-foreach/
 
+library(rbenchmark)
 library(foreach)
 library(doParallel)
 library(doMC)
 
-myInv <- function(n) {
-    m <- matrix(runif(n^2), nrow = n)
-    return(solve(m))
-}
-system.time(solve(matrix(runif(1000000), nrow = 1000)))
+r <- 100 # repetitions of task
+n <- 100 # number of rows in matrix
 
-foreach (i in 1:100){
-  
+myInv <- function(x) {
+    m <- matrix(runif(x^2), nrow = x)
+    return(solve(m)[1,])
 }
+
+#---
+# Time a loop
+#---
+  system.time({
+    for (i in 1:r){
+      myInv(n)  
+    }
+  })
+
+#---
+# Time an apply
+#---
+  system.time(sapply(1:r, function(x) myInv(n)))
+
+#---
+# Time the doParallel code
+#---
+
+  # First, try the code in serial
+    system.time(foreach(1:r) %do% myInv(n))
+
+  # from Unix prompt, can get number of processors from unix prompt -- -- cat /proc/cpuinfo | grep ^proc
+    registerDoMC(12)
+    system.time(x <- foreach(1:r) %dopar% myInv(n))
+
